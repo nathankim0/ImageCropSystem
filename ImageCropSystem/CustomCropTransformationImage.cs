468ade867661d5b94ee8a8b0e819d504e0a8e6ff
nathan9608 <jinyeob07@gmail.com>
2021-06-18 15:18:10 +0900

    Add Custom bindable CachedImage (options property)

---
+++ ImageCropSystem/CustomCropTransformationImage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FFImageLoading.Forms;
+using FFImageLoading.Transformations;
+using FFImageLoading.Work;
+using Xamarin.Forms;
+
+namespace ImageCropSystem
+{
+    public class CustomCropTransformationImage : CachedImage
+    {
+        public static BindableProperty OptionsProperty = BindableProperty.Create(nameof(Options), typeof(double[]), typeof(CustomCropTransformationImage), null, propertyChanged: UpdateOptions);
+
+        public double[] Options
+        {
+            get { return (double[])GetValue(OptionsProperty); }
+            set { SetValue(OptionsProperty, value); }
+        }
+        private static void UpdateOptions(BindableObject bindable, object oldValue, object newValue)
+        {
+            var _new = (double[])newValue;
+
+            var view = (CustomCropTransformationImage)bindable;
+            var transformations = new System.Collections.Generic.List<ITransformation>() {
+                    new CropTransformation()
+                    {
+                        XOffset = _new[0],
+                        YOffset = _new[1],
+                        CropHeightRatio =_new[2],
+                        CropWidthRatio = _new[3]
+                    }
+                };
+            view.Transformations = transformations;
+        }
+    }
+}
--- ImageCropSystem/MainPage.xaml.cs
+++ ImageCropSystem/MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using FFImageLoading.Transformations;
 using Xamarin.Forms;
 
 namespace ImageCropSystem
@@ -10,7 +9,30 @@ namespace ImageCropSystem
         public MainPage()
         {
             InitializeComponent();
-            crop.XOffset = image.Width / 2;
+
+         
+            image.SizeChanged += Image_SizeChanged;
         }
+
+        private void Image_SizeChanged(object sender, EventArgs e)
+        {
+            var w = image.Width;
+            var h = image.Height;
+
+            widthLabel.Text = w.ToString();
+            heightLabel.Text = h.ToString();
+
+            //crop.CropHeightRatio = 1;
+            //crop.CropWidthRatio = 1;
+            //crop.XOffset = w / 3;
+            //crop.YOffset = -h / 3;
+        }
+
+        //void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    var transformation = image.Transformations[0];
+        //    ((CropTransformation)transformation).CropHeightRatio = 2;
+            //((CropTransformation)transformation).CropWidthRatio = 1;
+        //}
     }
 }
